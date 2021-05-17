3c040189ad8e6d58a8aeb90e448253652aee6b1e
AlmasEgeubai <75420211+AlmasEgeubai@users.noreply.github.com>
2021-05-17 13:12:16 +0600

    Add files via upload

---
+++ src/eak04_sockets/startServer.java
@@ -0,0 +1,11 @@
+package eak04_sockets;
+
+import java.io.IOException;
+
+public class startServer {
+
+    public static void main(String[] args) throws IOException {
+        MyServer server = new MyServer();
+        server.start();
+    }
+}
